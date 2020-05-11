9d1c4f4cbe25f2eab0dbcbfa5df67fb817d27b07
Afams Val <myceet1@gmail.com>
2020-05-11 16:08:02 +0100

    updated middle ware

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 function jwtMiddleware(req, res, next) {
-  const token = req.header("authorization");
-  if (!token) res.json({ msg: "Authorization failed" });
+  const token = req.header("Authorization");
+
+  if (!token) return res.status(401).json({ msg: "Authorization failed" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (e) {
-    res.json({ msg: "Authorization failed" });
+    res.status(401).json({ msg: "Authorization failed.." });
   }
 }
 
